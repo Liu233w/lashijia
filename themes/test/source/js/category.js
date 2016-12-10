e83e2344e49a393ff37dd2e5ed0b037a52a81867
surface-of-liu <wwwlsmcom@outlook.com>
2016-12-10 10:15:05 +0800

    hotfix

--- themes/test/source/js/category.js
+++ themes/test/source/js/category.js
@@ -1,21 +1,28 @@
-(function( $ ) {
+(function($) {
 
-	var $container = $('.masonry-container');
+    var $container = $('.masonry-container');
     $container.masonry({
         columnWidth: '.item',
         itemSelector: '.item'
     });
-	
-	//Reinitialize masonry inside each panel after the relative tab link is clicked - 
-	$('a[data-toggle=tab]').each(function () {
-		var $this = $(this);
 
-		$this.on('shown.bs.tab', function () {
-		$container.masonry({
+    $container.imagesLoaded(function() {
+        $container.masonry({
             columnWidth: '.item',
             itemSelector: '.item'
         });
-		}); //end shown
-	});  //end each
-	
+    });
+
+    //Reinitialize masonry inside each panel after the relative tab link is clicked - 
+    $('a[data-toggle=tab]').each(function() {
+        var $this = $(this);
+
+        $this.on('shown.bs.tab', function() {
+            $container.masonry({
+                columnWidth: '.item',
+                itemSelector: '.item'
+            });
+        }); //end shown
+    }); //end each
+
 })(jQuery);
